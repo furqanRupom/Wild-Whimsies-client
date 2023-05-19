54593815ac0abfd069df94e4715a360c20803635
rupom <furqanrupom978@gmail.com>
2023-05-19 23:57:58 +0600

    fixed some issue on my toys pages

--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -5,12 +5,16 @@ import Swal from "sweetalert2";
 
 const MyToys = () => {
   const { user } = useContext(ToysContext);
+  console.log(user)
   const [myToys, setMyToys] = useState([]);
   useEffect(() => {
-    fetch(`http://localhost:5000/toys?${user?.email}`)
+    fetch(`http://localhost:5000/MyToys?sellerEmail=${user?.email}`)
       .then((res) => res.json())
-      .then((data) => setMyToys(data));
-  });
+      .then((data) => {
+        setMyToys(data)
+        console.log(data)
+      });
+  },[]);
 
   const deleteToysSubmit = (id)=>{
     console.log(id)
